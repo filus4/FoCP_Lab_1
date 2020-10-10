335e3f5f26e9768bb2c1e9b43d787f0960d2835c
marcel <filej1025@gmail.com>
2020-10-10 11:44:07 +0200

    Second commit - 'cin'

--- test.cpp
+++ test.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 int main(){
+    
+    int my_number;
 
-    std::cout << "Hello World!";
+    std::cin >> my_number;
+
+    std::cout << my_number << std::endl;
   
+    return 0;
 }
