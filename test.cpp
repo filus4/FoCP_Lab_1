4cab85068555cf7eb4c32cf099be4b3645bcb630
marcel <filej1025@gmail.com>
2020-10-10 12:08:02 +0200

    Fourth Commit - solving linear equation

--- test.cpp
+++ test.cpp
@@ -2,6 +2,31 @@
 
 int main(){
     
+    // ax + b = c
+    // ax = c - b
+    // x = (c - b) / a
+
+    float a;
+    float b;
+    float c;
+    
+    std::cout << "Please introduce a value for a:" << std::endl;
+
+    std::cin >> a;
+
+    std::cout << "Please introduce a value for b:" << std::endl;
+
+    std::cin >> b;
+
+    std::cout << "Please introduce a value for c:" << std::endl;
+
+    std::cin >> c;
+
+    float x = (c - b) / a;
+
+    std::cout << "The value of x is: " << x << std::endl;
+
+/*
     int my_number;
 
     std::cout << "Please introduce a number:" << std::endl;
@@ -13,7 +38,7 @@ int main(){
     } else {
         std:: cout << "This number is odd" << std::endl;
     }
-
+*/
     
     return 0;
 }
