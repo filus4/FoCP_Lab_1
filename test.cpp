50813b4c58de09e41702e69cadd6e17aec87855a
marcel <filej1025@gmail.com>
2020-10-10 11:53:18 +0200

    Third commit - 'if'

--- test.cpp
+++ test.cpp
@@ -4,9 +4,16 @@ int main(){
     
     int my_number;
 
+    std::cout << "Please introduce a number:" << std::endl;
+
     std::cin >> my_number;
 
-    std::cout << my_number << std::endl;
-  
+    if (my_number % 2 == 0){
+        std::cout << "This number is even" << std::endl;
+    } else {
+        std:: cout << "This number is odd" << std::endl;
+    }
+
+    
     return 0;
 }
